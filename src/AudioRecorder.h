8955f3d223c5be60a004b24e57520b6ff64bac58
Yacine Rezgui <rezgui.y@gmail.com>
2015-07-26 22:04:50 +0100

    Add AudioRecorder native module

---
+++ src/AudioRecorder.h
@@ -0,0 +1,13 @@
+//
+//  AudioRecorder.h
+//  meowth
+//
+//  Created by Yacine Rezgui on 26/07/2015.
+//  Copyright (c) 2015 Yacine Rezgui. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AudioRecorder : NSObject
+
+@end
---
+++ src/AudioRecorder.m
@@ -0,0 +1,13 @@
+//
+//  AudioRecorder.m
+//  meowth
+//
+//  Created by Yacine Rezgui on 26/07/2015.
+//  Copyright (c) 2015 Yacine Rezgui. All rights reserved.
+//
+
+#import "AudioRecorder.h"
+
+@implementation AudioRecorder
+
+@end
