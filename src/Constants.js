217a9e066af578ddc8015f1f8b08cb42e1c7dd64
Yacine Rezgui <rezgui.y@gmail.com>
2015-07-27 17:47:28 +0100

    Add constants

--- src/Constants.js
+++ src/Constants.js
@@ -3,6 +3,12 @@ module.exports = {
   STATUS: {
     WAITING: 'WAITING',
     RECORDING: 'RECORDING',
+    RECOGNIZING: 'RECOGNIZING',
+    DISPLAYING: 'DISPLAYING',
     PLAYING: 'PLAYING',
   },
+  API_URL: 'https://stream.watsonplatform.net/speech-to-text/api/v1/recognize?continuous=true',
+  MIME_TYPE: 'audio/l16; rate=48000; channels=2',
+  USERNAME: '12345-56789',
+  PASSWORD: 'secret',
 };
