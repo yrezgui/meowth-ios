eb9905158f691b79b649d12a25a0e5f92cfb0a45
Yacine Rezgui <rezgui.y@gmail.com>
2015-07-27 17:20:50 +0100

    Add HttpRequest native module

---
+++ src/HttpRequest.h
@@ -0,0 +1,13 @@
+//
+//  HttpRequest.h
+//  meowth
+//
+//  Created by Yacine Rezgui on 27/07/2015.
+//  Copyright (c) 2015 Yacine Rezgui. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HttpRequest : NSObject
+
+@end
---
+++ src/HttpRequest.m
@@ -0,0 +1,13 @@
+//
+//  HttpRequest.m
+//  meowth
+//
+//  Created by Yacine Rezgui on 27/07/2015.
+//  Copyright (c) 2015 Yacine Rezgui. All rights reserved.
+//
+
+#import "HttpRequest.h"
+
+@implementation HttpRequest
+
+@end
